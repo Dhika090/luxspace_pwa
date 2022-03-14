44fca4995ed0a16cc04948b0dbbcb7de32cf9ae2
dhika090 <nyes6953@gmail.com>
2022-03-14 23:22:23 +0700

    migration template & component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header.js';
+import Hero from './components/hero.js';
+import Browse from './components/browse.js';
+import Arrived from './components/arrived';
+import Clients from './components/clients';
+import Asidemenu from './components/asidemenu';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <> 
+    <Header />
+    <Hero />
+    <Browse />
+    <Arrived />
+    <Clients />
+    <Asidemenu />
+    <Footer />
+   </>
   );
 }
 
---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+function footer(){
+    return (
+        <footer className="flex justify-center px-4 py-8 text-center">
+        <p className="text-sm">
+          Copyright 2021 • All Rights Reserved LuxSpace by BuildWith Angga
+        </p>
+      </footer>
+    )
+}
+export default footer;
